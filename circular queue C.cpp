ce5b73baf3351db939b3799d76ee0fd15b1aceef
Muhammed Nour <118080466+MUHAMMEDMAME@users.noreply.github.com>
2024-01-30 19:07:26 +0300

    Add files via upload

--- circular queue C.cpp
+++ circular queue C.cpp
@@ -1,67 +1,85 @@
-#include <stdio.h>
-#include <stdlib.h>
-struct Queue
-{
+#include<iostream>
+using namespace std;
+struct Queue {
 	int size;
 	int front;
 	int rear;
 	int* Q;
 };
-void create(struct Queue* q, int size)
-{
-	q->size = size;
-	q->front = q->rear = 0;
-	q->Q = (int*)malloc(q->size * sizeof(int));
-}
-void enqueue(struct Queue* q, int x)
-{
-	if ((q->rear + 1) % q->size == q->front)
-		printf("Queue is Full");
-	else
-	{
-		q->rear = (q->rear + 1) % q->size;
-		q->Q[q->rear] = x;
-	}
-}
-int dequeue(struct Queue* q)
-{
-	int x = -1;
+//create queue
+void create(Queue* q, int size);
+//insert new elemnt
+void enqueue(Queue* q,int x);
+//delete element
+int dequeue(Queue* q);
 
-	if (q->front == q->rear)
-		printf("Queue is Empty\n");
-	else
-	{
-		q->front = (q->front + 1) % q->size;
-		x = q->Q[q->front];
-	}
-	return x;
-}
-void Display(struct Queue q)
-{
-	int i = q.front + 1;
+// queue is Empty return true else false
+bool isEmpty(Queue* q);
 
-	do
-	{
+//queue is Full return true else return true
+bool isFull(Queue* q);
 
-		printf("%d ", q.Q[i]);
-		i = (i + 1) % q.size;
-	} while (i != (q.rear + 1) % q.size);
 
-	printf("\n");
-}
-int main()
-{
-	struct Queue q;
+//Display queue
+void Display(Queue q);
+int main() {
+	Queue q;
 	create(&q, 5);
-
 	enqueue(&q, 10);
 	enqueue(&q, 20);
 	enqueue(&q, 30);
 	enqueue(&q, 40);
-	
-
+	cout << dequeue(&q)<<endl;
 	Display(q);
 
-	printf("%d ", dequeue(&q));
-	return 0;
 }
+void create(Queue* q, int size) {
+	q->size = size;
+	q->rear = q->front = 0;
+	q->Q = (int*)malloc(sizeof(int) * q->size);
+}
+
+void enqueue(Queue* q,int x) {
+	if (isFull(q))
+		cout << "queue is full\n";
+	else
+	{
+		q->rear = (q->rear + 1) % q->size;
+		q->Q[q->rear] = x;
+
+	}
+}
+
+int dequeue(Queue* q){
+	int x = -1;
+	if (q->front == q->rear)
+		return x;
+	else
+	{
+		q->front = (q->front + 1) % q->size;
+		x = q->Q[q->front];
+
+	}
+
+	return x;
+}
+bool isEmpty(Queue* q) {
+	if (q->rear == q->front)
+		return true;
+	return false;
+}
+bool isFull(Queue* q) {
+	if ((q->rear + 1) % q->size == q->front)
+		return true;
+	return false;
+}
+
+void Display(Queue q) {
+	int i = q.front + 1;
+	do {
+		cout << q.Q[i]<<" ";
+		i = (i + 1) % q.size;
+
+	} while (i != (q.rear + 1) % q.size);
+}
+
